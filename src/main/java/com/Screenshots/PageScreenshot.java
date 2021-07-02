bc8285e85d9cef69ea89302903313e9a05687962
mohsinfarabi <mohsin.farabi@gmail.com>
2021-07-02 18:02:33 +0600

    Implementing AShot Library

--- src/main/java/com/Screenshots/PageScreenshot.java
+++ src/main/java/com/Screenshots/PageScreenshot.java
@@ -4,15 +4,21 @@ import com.Base.TestBase;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class PageScreenshot extends TestBase {
     public static void main(String[] args) throws IOException {
         chromeLaunch();
-        openURL("https://google.com");
-        screenShot("Page Image");
+        openURL("https://demo.opencart.com/");
+        //screenShot("Page Image");
+        fullscreenShot("Full Page");
         closeBrowser();
 
     }
@@ -23,5 +29,15 @@ public class PageScreenshot extends TestBase {
 
         //Save image
         FileUtils.copyFile(srcFile,new File(System.getProperty("user.dir")+"//src//main//Screenshots//"+name+".png"), true);
+    }
+
+    public static void  fullscreenShot(String name) throws IOException {
+        //Take Screenshot
+
+        Screenshot fullPage = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
+
+        //Save Image
+        ImageIO.write(fullPage.getImage(),"png", new File(System.getProperty("user.dir")+"//src//main//Screenshots//"+name+".png"));
+
     }
 }
--- src/main/java/com/pdf/GeneratePdf.java
+++ src/main/java/com/pdf/GeneratePdf.java
@@ -5,8 +5,15 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+import ru.yandex.qatools.ashot.util.ImageTool;
 
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -14,7 +21,8 @@ public class GeneratePdf extends TestBase {
     public static void main(String[] args) throws DocumentException, IOException {
         firefoxLaunch();
         openURL("https://demo.opencart.com/");
-        createPDF();
+        //createPDF();
+        createPDFFullImage();
         closeBrowser();
     }
 
@@ -37,6 +45,41 @@ public class GeneratePdf extends TestBase {
         Image img= Image.getInstance(input);
 
         img.scaleToFit(PageSize.A4.getWidth()/2,PageSize.A4.getHeight()/2);
+
+        doc.add(img);
+
+        doc.add(new Paragraph(""));
+
+        doc.close();
+        writer.close();
+    }
+
+    public static void createPDFFullImage() throws IOException, DocumentException {
+
+        //take screenshot and save as byte[]
+
+        Screenshot fullPage = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
+
+
+        byte[] input = ImageTool.toByteArray(fullPage.getImage()); //AShot ImageTool Method
+
+        //byte[] input = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+
+        Document doc = new Document(); // itextpdf Document
+
+        String output = System.getProperty("user.dir")+"//src//main//PDF//NewPdfFull.pdf";
+
+        FileOutputStream fs = new FileOutputStream(output);
+
+        PdfWriter writer = PdfWriter.getInstance(doc,fs);
+
+        //open pdf for writing
+        writer.open();
+        doc.open();
+
+        Image img= Image.getInstance(input);
+
+        img.scaleToFit(PageSize.A4.getWidth(),PageSize.A4.getHeight());
 
         doc.add(img);
 
